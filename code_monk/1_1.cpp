d30f1f38c741a8ff1b08e8e9fe4f1611a3d8a24d
Akshay Nagpal <akshay2626@gmail.com>
2015-12-28 18:55:45 +0530

    started codemonk

---
+++ code_monk/1_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s,s1;
+	int n;
+	cin>>n;
+	
+	for(int k=0;k<n;k++)
+	{
+		cin>>s;
+		s1="";
+		for(int i=s.size()-1;i>=0;i--)
+		{
+			s1 += s[i];
+		}
+		cout<<s1<<endl;
+	}
+  
+    return 0;
+}
